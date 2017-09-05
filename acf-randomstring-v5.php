6bc47aafaa3fe6dd0c37ebfd3146601564ea6cf3
regenrek <hello@regenrek.at>
2017-09-05 17:32:06 +0200

    added fixes

--- acf-randomstring-v5.php
+++ acf-randomstring-v5.php
@@ -1,8 +1,8 @@
 <?php
 
 class acf_field_randomstring extends acf_field {
-	
-	
+
+
 	/*
 	*  __construct
 	*
@@ -15,61 +15,67 @@ class acf_field_randomstring extends acf_field {
 	*  @param	n/a
 	*  @return	n/a
 	*/
-	
+
 	function __construct() {
-		$characters = ’23456789ABCDEFGHJKLMNPQRSTUVWXYZ’;
-		$randstring = '';
-		for ($i = 0; $i < 6; $i++) {
-			$randstring .= $characters[rand(0, strlen($characters))];
-		}
-		
-		
+
+
 		/*
 		*  name (string) Single word, no spaces. Underscores allowed
 		*/
-		
+
 		$this->name = 'randomstring';
-		
-		
+
+
 		/*
 		*  label (string) Multiple words, can include spaces, visible when selecting a field type
 		*/
-		
+
 		$this->label = __('Random String', 'acf-randomstring');
-		
-		
+
+
 		/*
 		*  category (string) basic | content | choice | relational | jquery | layout | CUSTOM GROUP NAME
 		*/
-		
+
 		$this->category = 'basic';
-		
-		
+
+
 		/*
 		*  defaults (array) Array of default settings which are merged into the field object. These are used later in settings
 		*/
-		
+
 		$this->defaults = array(
-			'string'	=> $randstring,
+			'string'	=> null,
 		);
-		
-		
+
+
 		/*
 		*  l10n (array) Array of strings that are used in JavaScript. This allows JS strings to be translated in PHP and loaded via:
 		*  var message = acf._e('randomstring', 'error');
 		*/
-		
+
 		$this->l10n = array(
 			'error'	=> __('Error! Please enter a higher value', 'acf-randomstring'),
 		);
-		
-				
+
+
 		// do not delete!
     	parent::__construct();
-    	
+
 	}
-	
-	
+
+
+	function getToken() {
+    $characters = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+    $randstring = '';
+    for ($i = 0; $i < 6; $i++) {
+      $randstring .= $characters[rand(0, strlen($characters))];
+    }
+
+    return strtolower($randstring);
+  }
+
+
 	/*
 	*  render_field_settings()
 	*
@@ -82,9 +88,9 @@ class acf_field_randomstring extends acf_field {
 	*  @param	$field (array) the $field being edited
 	*  @return	n/a
 	*/
-	
+
 	function render_field_settings( $field ) {
-		
+
 		/*
 		*  acf_render_field_setting
 		*
@@ -94,7 +100,7 @@ class acf_field_randomstring extends acf_field {
 		*  More than one setting can be added by copy/paste the above code.
 		*  Please note that you must also have a matching $defaults value for the field name (font_size)
 		*/
-		
+
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Random String','acf-randomstring'),
 			'instructions'	=> __('Random generated string','acf-randomstring'),
@@ -104,9 +110,9 @@ class acf_field_randomstring extends acf_field {
 		));
 
 	}
-	
-	
-	
+
+
+
 	/*
 	*  render_field()
 	*
@@ -121,30 +127,20 @@ class acf_field_randomstring extends acf_field {
 	*  @param	$field (array) the $field being edited
 	*  @return	n/a
 	*/
-	
+
 	function render_field( $field ) {
-		
-		
-		/*
-		*  Review the data of $field.
-		*  This will show what data is available
-		*/
-		
-		echo '<pre>';
-			print_r( $field );
-		echo '</pre>';
-		
-		
-		/*
-		*  Create a simple text input using the 'font_size' setting.
-		*/
-		
-		?>
-		<input type="text" name="<?php echo esc_attr($field['name']) ?>" value="<?php echo esc_attr($field['value']) ?>" />
-		<?php
+		// CSS to hide the field and its wrapper
+    ?>
+    <style
+        type="text/css">.field_key-<?php echo $field['key']; ?>, .acf-<?php echo str_replace('_', '-', $field['key']); ?>, .acf-field-<?php echo str_replace('_', '-', $field['key']); ?>, [data-key="<?php echo $field['key']; ?>"] {
+        display: none;
+      }</style>
+    <input type="hidden" name="<?php echo esc_attr($field['name']) ?>" value="<?php echo esc_attr($field['value']) ?>"
+           style="display: none"/>
+    <?php
 	}
-	
-		
+
+
 	/*
 	*  input_admin_enqueue_scripts()
 	*
@@ -160,27 +156,27 @@ class acf_field_randomstring extends acf_field {
 	*/
 
 	/*
-	
+
 	function input_admin_enqueue_scripts() {
-		
+
 		$dir = plugin_dir_url( __FILE__ );
-		
-		
+
+
 		// register & include JS
 		wp_register_script( 'acf-input-randomstring', "{$dir}js/input.js" );
 		wp_enqueue_script('acf-input-randomstring');
-		
-		
+
+
 		// register & include CSS
-		wp_register_style( 'acf-input-randomstring', "{$dir}css/input.css" ); 
+		wp_register_style( 'acf-input-randomstring', "{$dir}css/input.css" );
 		wp_enqueue_style('acf-input-randomstring');
-		
-		
+
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  input_admin_head()
 	*
@@ -196,21 +192,21 @@ class acf_field_randomstring extends acf_field {
 	*/
 
 	/*
-		
+
 	function input_admin_head() {
-	
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
    	*  input_form_data()
    	*
    	*  This function is called once on the 'input' page between the head and footer
-   	*  There are 2 situations where ACF did not load during the 'acf/input_admin_enqueue_scripts' and 
+   	*  There are 2 situations where ACF did not load during the 'acf/input_admin_enqueue_scripts' and
    	*  'acf/input_admin_head' actions because ACF did not know it was going to be used. These situations are
    	*  seen on comments / user edit forms on the front end. This function will always be called, and includes
    	*  $args that related to the current screen such as $args['post_id']
@@ -222,18 +218,18 @@ class acf_field_randomstring extends acf_field {
    	*  @param	$args (array)
    	*  @return	n/a
    	*/
-   	
+
    	/*
-   	
+
    	function input_form_data( $args ) {
-	   	
-		
-	
+
+
+
    	}
-   	
+
    	*/
-	
-	
+
+
 	/*
 	*  input_admin_footer()
 	*
@@ -249,16 +245,16 @@ class acf_field_randomstring extends acf_field {
 	*/
 
 	/*
-		
+
 	function input_admin_footer() {
-	
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  field_group_admin_enqueue_scripts()
 	*
@@ -274,14 +270,14 @@ class acf_field_randomstring extends acf_field {
 	*/
 
 	/*
-	
+
 	function field_group_admin_enqueue_scripts() {
-		
+
 	}
-	
+
 	*/
 
-	
+
 	/*
 	*  field_group_admin_head()
 	*
@@ -297,11 +293,11 @@ class acf_field_randomstring extends acf_field {
 	*/
 
 	/*
-	
+
 	function field_group_admin_head() {
-	
+
 	}
-	
+
 	*/
 
 
@@ -319,18 +315,18 @@ class acf_field_randomstring extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$value
 	*/
-	
-	
-	
+
+
+
 	function load_value( $value, $post_id, $field ) {
-		
+
 		return trim($value);
-		
+
 	}
-	
-	
-	
-	
+
+
+
+
 	/*
 	*  update_value()
 	*
@@ -345,18 +341,19 @@ class acf_field_randomstring extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$value
 	*/
-	
-	
-	
+
+
+
 	function update_value( $value, $post_id, $field ) {
-		
-		return trim($value);
-		
+		if($value === null) {
+			$value = $this->getToken();
+		}
+		return $value;
 	}
-	
-	
-	
-	
+
+
+
+
 	/*
 	*  format_value()
 	*
@@ -372,35 +369,35 @@ class acf_field_randomstring extends acf_field {
 	*
 	*  @return	$value (mixed) the modified value
 	*/
-		
+
 	/*
-	
+
 	function format_value( $value, $post_id, $field ) {
-		
+
 		// bail early if no value
 		if( empty($value) ) {
-		
+
 			return $value;
-			
+
 		}
-		
-		
+
+
 		// apply setting
-		if( $field['font_size'] > 12 ) { 
-			
+		if( $field['font_size'] > 12 ) {
+
 			// format the value
 			// $value = 'something';
-		
+
 		}
-		
-		
+
+
 		// return
 		return $value;
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  validate_value()
 	*
@@ -418,33 +415,33 @@ class acf_field_randomstring extends acf_field {
 	*  @param	$input (string) the corresponding input name for $_POST value
 	*  @return	$valid
 	*/
-	
+
 	/*
-	
+
 	function validate_value( $valid, $value, $field, $input ){
-		
+
 		// Basic usage
 		if( $value < $field['custom_minimum_setting'] )
 		{
 			$valid = false;
 		}
-		
-		
+
+
 		// Advanced usage
 		if( $value < $field['custom_minimum_setting'] )
 		{
 			$valid = __('The value is too little!','acf-randomstring'),
 		}
-		
-		
+
+
 		// return
 		return $valid;
-		
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  delete_value()
 	*
@@ -459,18 +456,18 @@ class acf_field_randomstring extends acf_field {
 	*  @param	$key (string) the $meta_key which the value was deleted
 	*  @return	n/a
 	*/
-	
+
 	/*
-	
+
 	function delete_value( $post_id, $key ) {
-		
-		
-		
+
+
+
 	}
-	
+
 	*/
-	
-	
+
+
 	/*
 	*  load_field()
 	*
@@ -478,23 +475,23 @@ class acf_field_randomstring extends acf_field {
 	*
 	*  @type	filter
 	*  @date	23/01/2013
-	*  @since	3.6.0	
+	*  @since	3.6.0
 	*
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$field
 	*/
-	
+
 	/*
-	
+
 	function load_field( $field ) {
-		
+
 		return $field;
-		
-	}	
-	
+
+	}
+
 	*/
-	
-	
+
+
 	/*
 	*  update_field()
 	*
@@ -507,18 +504,18 @@ class acf_field_randomstring extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	$field
 	*/
-	
+
 	/*
-	
+
 	function update_field( $field ) {
-		
+
 		return $field;
-		
-	}	
-	
+
+	}
+
 	*/
-	
-	
+
+
 	/*
 	*  delete_field()
 	*
@@ -531,18 +528,18 @@ class acf_field_randomstring extends acf_field {
 	*  @param	$field (array) the field array holding all the field options
 	*  @return	n/a
 	*/
-	
+
 	/*
-	
+
 	function delete_field( $field ) {
-		
-		
-		
-	}	
-	
+
+
+
+	}
+
 	*/
-	
-	
+
+
 }
 
 
